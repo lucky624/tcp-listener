4a1ac22258bd63695e6db149728b5c959624dbc0
lucky624 <lironnanda@gmail.com>
2020-04-20 18:05:19 +0600

    add flags extractor

---
+++ extractor.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+import os, re
+from storage import storage_set, storage_get
+
+REDIS_STORAGE = 1 # 1 - on
+                  # 0 - off
+
+pwd_files = []
+
+for d, dirs, files in os.walk('data'):
+    pwd_files = files
+    break
+
+if REDIS_STORAGE == 1:
+    for file in pwd_files:
+        if not storage_get(file):
+            f = open(d + '/' + file, 'rb')
+            for line in f.readlines():
+                try:
+                    decode_line = line.decode()
+                    flags = re.findall("[A-Z0-9]{31}=", decode_line)
+                    if flags:
+                        for flag in flags:
+                            print(flag, flush=True)
+                except:
+                    continue
+            f.close()
+            storage_set(file)
+else:
+    for file in pwd_files:
+        f = open(file, 'rb')
+        for line in f.readlines():
+            try:
+                decode_line = line.decode()
+                flags = re.findall("[A-Z0-9]{31}=", decode_line)
+                if flags:
+                    for flag in flags:
+                        print(flag, flush=True)
+            except:
+                continue
+        f.close()
---
+++ start_extractor.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./start_sploit.py extractor.py --server-url http://192.168.0.100 --not-per-team --pool-size 1 --attack-period 5
